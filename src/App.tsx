610ee51a48c2ef3f700cd7b93f73d90f9e829016
mehra <mehradotdev@gmail.com>
2025-09-05 07:39:01 +0530

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="text-md font-bold underline text-lime-500">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
