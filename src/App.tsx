ed711c455878ed383427adcf18de7e07b6721f6c
mehra <mehradotdev@gmail.com>
2025-09-05 07:58:18 +0530

    chore: add changes suggested by coderabbit

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="text-md font-bold underline text-lime-500">
+      <p className="text-base font-bold underline text-lime-500">
         Click on the Vite and React logos to learn more
       </p>
     </>
